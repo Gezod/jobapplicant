167903011a61feeb10579fd0b9a202189fc86b22
Gezod <refanggalintar@gmail.com>
2023-07-09 08:21:42 +0700

    first commit

---
+++ thanks.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Thank you for sumbitting</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144808195-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-144808195-1');
+	</script>
+</head>
+
+
+
+<body style="font-family:verdana">
+
+<div class="jumbotron text-xs-center">
+  <h1 class="display-3">Thank You!</h1>
+  <p class="lead"><strong>Untuk Pelamar Terima Kasih Telah Mengisi Data.</strong> Silahkan Cek Email bagian New Inbox untuk mengetahui informasi lebih lanjut
+<br>Thank you</p>
+  <hr>
+  <p>
+    Ingin mendaftar lagi <a href="admin">Silahkan pergi ke laman dashboard</a>
+  </p>
+  <p class="lead">
+	<a  class="btn btn-primary btn-sm" href="index.php" role="button">Kembai ke Job</a>
+  </p>
+</div>
+
+
+</body>
+</html>
